b067940b6a8e2eca9e1e79e578dc6e8ab9858af2
Mauricio06Everis <kpalmall@everis.com>
2021-08-29 17:08:26 -0500

    Config AZURE

--- src/main/java/com/everis/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/everis/gatewayservice/GatewayServiceApplication.java
@@ -3,7 +3,6 @@ package com.everis.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -11,7 +10,6 @@ public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
-		
 	}
 
 }
